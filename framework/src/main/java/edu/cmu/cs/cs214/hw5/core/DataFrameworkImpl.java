48b3c2d88643b7ce4ddcab98fbe269a42d65377d
Nekhera <32021246+Nekhera@users.noreply.github.com>
2019-04-07 17:16:46 -0400

    Added runDataPlugin method to DataFrameworkImpl

--- framework/src/main/java/edu/cmu/cs/cs214/hw5/core/DataFrameworkImpl.java
+++ framework/src/main/java/edu/cmu/cs/cs214/hw5/core/DataFrameworkImpl.java
@@ -1,5 +1,10 @@
 package edu.cmu.cs.cs214.hw5.core;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * The framework core implementation.
  */
@@ -28,6 +33,16 @@ public class DataFrameworkImpl {
 
     public void displayData() {
         displayPlugin.visualize(displayDataStructure);
+    }
+
+    public Map<String, Map<String, Map<Integer, BigDecimal>>> runDataPlugin(List<DataPoint> dataPointList) {
+        Map<String, Map<String, Map<Integer, BigDecimal>>> tree = new TreeMap<>();
+        for (DataPoint dataPoint : dataPointList) {
+            tree.computeIfAbsent(dataPoint.getState(), k -> new TreeMap<>());
+            tree.get(dataPoint.getState()).computeIfAbsent(dataPoint.getCounty(), k -> new TreeMap<>());
+            tree.get(dataPoint.getState()).get(dataPoint.getCounty()).put(dataPoint.getStartDate(), dataPoint.getValue());
+        }
+        return tree;
     }
 }
 
