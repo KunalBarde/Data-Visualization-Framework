9bd86063297f63193ebcbd1a9538c7be4c966f3c
Nekhera <32021246+Nekhera@users.noreply.github.com>
2019-04-09 21:43:20 -0400

    Commit

--- plugins/src/main/java/edu/cmu/cs/cs214/hw5/plugins/MapDisplayPlugin.java
+++ plugins/src/main/java/edu/cmu/cs/cs214/hw5/plugins/MapDisplayPlugin.java
@@ -2,37 +2,93 @@ package edu.cmu.cs.cs214.hw5.plugins;
 
 import edu.cmu.cs.cs214.hw5.framework.core.DisplayDataStructure;
 import edu.cmu.cs.cs214.hw5.framework.core.DisplayPlugin;
+import edu.cmu.cs.cs214.hw5.framework.core.PData;
 
 import javax.imageio.ImageIO;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class MapDisplayPlugin implements DisplayPlugin {
+    private PData pData;
+
     public JPanel visualize(DisplayDataStructure displayDataStructure) {
-        final BufferedImage image;
-        try {
-             image = ImageIO.read(new File("src/main/resources/Connecticut.png"));
-             JPanel panel = new JPanel() {
-                 @Override
-                 protected void paintComponent(Graphics g) {
-                     super.paintComponent(g);
-                     g.drawImage(image, 0, 0, null);
-                 }
-             };
-             panel.setPreferredSize(new Dimension(800, 583));
-             return panel;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        pData = displayDataStructure.processFilterData(null);
+
+        JPanel panel = new JPanel();
+        JPanel imagePanel = new JPanel();
+
+        imagePanel.setPreferredSize(new Dimension(800, 583));
+        panel.setLayout(new BorderLayout());
+        panel.add(imagePanel, BorderLayout.NORTH);
+
+        List<Integer> timeRange = displayDataStructure.getTimeRanges();
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, timeRange.get(0), timeRange.get(timeRange.size() - 1), timeRange.get(0));
+        slider.setMajorTickSpacing(5);
+        slider.setMinorTickSpacing(1);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        panel.add(slider, BorderLayout.SOUTH);
+
+        return panel;
     }
 
     public String getName() {
         return "Connecticut Map";
+    }
+
+    private class ImagePanel extends JPanel {
+        BufferedImage image;
+
+        String middlesex;
+        String fairfield;
+        String windham;
+        String newHaven;
+        String tolland;
+        String newEngland;
+        String litchfield;
+        String hartford;
+
+        ImagePanel() {
+            try {
+                image = ImageIO.read(new File("src/main/resources/Connecticut.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            updatePanel();
+        }
+
+        void updatePanel() {
+            middlesex = pData.getCountyData("CT", "Middlesex").get();
+            fairfield = pData.getCountyData("CT", "Fairfield").get();
+            windham = pData.getCountyData("CT", "Windham").get();
+            newHaven = pData.getCountyData("CT", "New Haven").get();
+            tolland = pData.getCountyData("CT", "Tolland").get();
+            newEngland = pData.getCountyData("CT", "New England").get();
+            litchfield = pData.getCountyData("CT", "Litchfield").get();
+            hartford = pData.getCountyData("CT", "Hartford").get();
+
+            this.repaint();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(image, 0, 0, null);
+            g.drawString(middlesex, 300, 400);
+            g.drawString(fairfield, 300, 400);
+            g.drawString(newEngland, 300, 400);
+            g.drawString(newHaven, 300, 400);
+            g.drawString(tolland, 300, 400);
+            g.drawString(hartford, 300, 400);
+            g.drawString(windham, 300, 400);
+            g.drawString(litchfield, 300, 400);
+        }
     }
 }
